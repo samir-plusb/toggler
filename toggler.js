15ed81c2e39e527bd65fb913e718675d05f10e59
Samir Rachidi <sr@plusb.de>
2023-05-04 10:38:07 +0200

    Added toggler.js and toggler.css

---
+++ toggler.js
@@ -0,0 +1,29 @@
+export default class Toggler {
+    constructor() {
+        this.initToggler();
+    }
+
+    initToggler() {
+        const inputs = document.querySelectorAll('[data-toggle-target]');
+
+        inputs.forEach((input) => {
+            input.addEventListener('input', (event) => {
+                this.handleInput(event.target);
+            });
+        });
+    }
+
+    handleInput(input) {
+        const toggleTarget = input.dataset.toggleTarget;
+        const toggleValue = input.dataset.toggleValue;
+        const targetElements = document.querySelectorAll(toggleTarget);
+
+        targetElements.forEach((element) => {
+            if (input.value === toggleValue) {
+                element.classList.remove('toggler-hidden');
+            } else {
+                element.classList.add('toggler-hidden');
+            }
+        });
+    }
+}
